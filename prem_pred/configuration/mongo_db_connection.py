ba941605d4d91901a88debbe10ec1222fad59502
vadirajkrishna <vadirajkrishna@gmail.com>
2024-07-06 10:05:50 +0530

    updated the secret

--- prem_pred/configuration/mongo_db_connection.py
+++ prem_pred/configuration/mongo_db_connection.py
@@ -23,7 +23,7 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) -> None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url = os.getenv(MONGODB_URL_KEY)
+                mongo_db_url = os.getenv(MONGODB_URL_KEY) #Read the CS
                 if mongo_db_url is None:
                     raise Exception(f"Environment key: {MONGODB_URL_KEY} is not set.")
                 MongoDBClient.client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
